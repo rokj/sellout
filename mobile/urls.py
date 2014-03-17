65c08450d9873b5229aa9ff029b458bd1b5d1906
tomaz <tomaz.znideric@blocklogic.net>
2014-03-17 10:28:18 +0100

    error fixed

--- mobile/urls.py
+++ mobile/urls.py
@@ -29,12 +29,8 @@ urlpatterns = patterns('',
     url(r'^mobile-login/(?P<backend>[\w-]+)$', login.obtain_auth_token),
 
 
-    # management urls: company
-    url(r_company + _('/manage') + '/?$', manage.manage_home, name='manage_home'), # management home
-    url(r_company + _('/manage/company') + '/?$', company.edit_company, name='edit_company'), # company
 
     # categories
-    #url(r_company + r'/manage/json/category/add/(?P<parent_id>-?\d+)/?$', category.mobile_add_category, name='add_category'), # add
     url(r_company + r'/manage/json/category/get/(?P<category_id>\d+)/?$', category.mobile_get_category, name='get_category'),
     url(r_company + r'/manage/json/category/add/(?P<parent_id>-?\d+)/?$', category.mobile_add_category, name='add_category'),
     url(r_company + r'/manage/json/category/edit/(?P<category_id>\d+)/?$', category.mobile_edit_category, name='edit_category'), # edit
@@ -46,33 +42,21 @@ urlpatterns = patterns('',
     url(r_company + r'/manage/json/contact/add/?$', contact.mobile_add_contact, name='add_contact'),
     url(r_company + r'/manage/json/contact/get/(?P<contact_id>\d+)/?$', contact.mobile_get_contact, name='get_contact'),
     url(r_company + _('/manage/json/contact/edit') + '/(?P<contact_id>\d+)/?$', contact.mobile_edit_contact, name='edit_contact'),
-    url(r_company + _('/manage/contact/delete') + '/(?P<contact_id>\d+)/?$', contact.delete_contact, name='delete_contact'),
+    url(r_company + _('/manage/contact/delete') + '/(?P<contact_id>\d+)/?$', contact.mobile_delete_contact, name='delete_contact'),
     
     # taxes
     url(r_company + _('/manage/taxes') + '/?$', tax.mobile_list_taxes, name='list_taxes'), # template view
     url(r_company + r'/manage/json/taxes/?$', tax.mobile_get_taxes, name='get_taxes'), # get all taxes in a json list
     url(r_company + r'/manage/json/taxes/save/?$', tax.mobile_save_taxes, name='save_taxes'), # save (override existing) taxes
 
-    
-    # discounts
-    url(r_company + _('/manage/discounts') + '/?$', discount.list_discounts, name='list_discounts'),
-    url(r_company + _('/manage/discount/add') + '/?$', discount.add_discount, name='add_discount'),
-    url(r_company + _('/manage/discount/edit') + '/(?P<discount_id>\d+)/?$', discount.edit_discount, name='edit_discount'),
-    url(r_company + _('/manage/discount/delete') + '/(?P<discount_id>\d+)/?$', discount.delete_discount, name='delete_discount'),
 
     # products
-    url(r_company + _('/manage/products') + '/?$', product.products, name='products'), # static (template) page
     url(r_company + r'/manage/json/products/search/?$', product.mobile_search_products, name='search_products'), # product list (search) - json
     url(r_company + r'/manage/json/products/add/?$', product.mobile_create_product, name='mobile_create_product'), # edit (save) product - json
     url(r_company + r'/manage/json/products/get/(?P<product_id>\d+)/?$', product.mobile_get_product, name='get_product'), # product list (search) - json
     url(r_company + r'/manage/json/products/edit/(?P<product_id>\d+)/?$', product.mobile_edit_product, name='edit_product'), # edit (save) product - json
     url(r_company + r'/manage/json/products/delete/(?P<product_id>\d+)/?$', product.delete_product, name='delete_product'), # edit (save) product - json
-    # users
-    #url(r_company + _('/manage/users') + '/?$', manage.users.edit_company, name='edit_users'), # company
-    # config (company parameter is needed only for url; configuration is per user, regardless of company
-    url(r_company + _('/manage/configuration') + '/?$', configuration.edit_config, name='edit_config'), # company
-    
-    
+
     # misc (ajax): urls not translated
     
     
@@ -84,6 +68,6 @@ urlpatterns = patterns('',
     url(r_company + r'/manage/json/units/?$', product.mobile_JSON_units, name='mobile_JSON_units'),
     
     # available discounts list
-    url(r_company + r'/manage/json/discounts/?$', discount.JSON_discounts, name='JSON_discounts'),
+    # url(r_company + r'/manage/json/discounts/?$', discount.JSON_discounts, name='JSON_discounts'),
     
 )
