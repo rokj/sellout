562abb73dea694c8d5f9edaa4179b93dbe21b330
Rok Jaklič <rokj@rasca.net>
2014-11-18 12:53:33 +0100

    res taprava verzija

--- common/models.py
+++ common/models.py
@@ -1,7 +1,6 @@
 import logging
 from django.db.models.signals import post_save, post_delete
 from django.db import models
-from django.contrib.auth.models import User
 from django.dispatch import receiver
 from django.utils.translation import ugettext as _
 from django.utils.timezone import now
