65d9a3e06b23ffd389eb490df77e08cc4622dcb1
Nejc Jurkovič <nejc.jurkovic@blocklogic.net>
2014-11-27 11:47:44 +0100

    checking rok's work

--- action/models.py
+++ action/models.py
@@ -1,15 +1,16 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
-from common.globals import WAITING, ACTION_STATUS
+from common.globals import WAITING
 
 from common.models import SkeletonU
+import common.globals as g
 
 
 class Action(SkeletonU):
     _for = models.EmailField(_("For whom"), blank=False, null=False, db_index=True)
     what = models.CharField(_("For what"), help_text=_("Should be just code"), db_index=True,
                             max_length=100, blank=False, null=False)
-    status = models.CharField(_("Waiting action status"), default=WAITING, choices=ACTION_STATUS,
+    status = models.CharField(_("Waiting action status"), default=WAITING, choices=g.ACTION_STATUS_CHOICES,
                               max_length=30, null=False, blank=False)
     data = models.TextField(_("Additional data"))
     reference = models.CharField(_("Reference for an action"), max_length=30, null=False, blank=False, unique=True)
