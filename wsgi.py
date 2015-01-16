76faa89d25c16d0ea8bd8a84a1e8ae9635fd5d30
Rok Jaklic <rokj@rasca.net>
2015-01-16 13:40:02 +0100

    from PIL import ...

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for webpos project.
+WSGI config for taskmanager project.
 
 This module contains the WSGI application used by Django's development server
 and any production WSGI deployments. It should expose a module-level variable
@@ -13,15 +13,26 @@ middleware here, or combine a Django application with an application of another
 framework.
 
 """
-import os
+import os, sys
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "webpos.settings")
+# We defer to a DJANGO_SETTINGS_MODULE already in the environment. This breaks
+# if running multiple sites in the same mod_wsgi process. To fix this, use
+# mod_wsgi daemon mode with each site in its own daemon process, or use
+# os.environ["DJANGO_SETTINGS_MODULE"] = "taskmanager.settings"
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sellout_biz.settings")
+sys.path.append(os.path.abspath(os.path.dirname(__file__)))
+sys.path.append(os.path.join(os.path.realpath(os.path.dirname(__file__)), '../../../'))
+sys.path.append(os.path.join(os.path.realpath(os.path.dirname(__file__)), '../../'))
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'sellout_biz.settings'
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+
+# application = django.core.wsgi:get_wsgi_application()
 
 # Apply WSGI middleware here.
 # from helloworld.wsgi import HelloWorldApplication
