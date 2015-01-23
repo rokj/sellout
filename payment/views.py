7efc6e101cabfd21445b0076c98cc5464c6a0016
Rok Jaklič <rokj@rasca.net>
2015-01-23 13:39:10 +0100

    sweet mother of paypal

--- payment/views.py
+++ payment/views.py
@@ -1,7 +1,9 @@
 # -*- coding:utf-8 -*-
-from django.http import HttpResponse, Http404
+from urllib2 import urlopen
+from django.http import HttpResponse, Http404, QueryDict
 from django.views.decorators.csrf import csrf_exempt
 import requests
+from six import b
 from common.globals import PAID
 from pos.models import Bill
 import settings
@@ -15,13 +17,23 @@ class AfterResponse(HttpResponse):
     request = None
     logger = logging.getLogger(__name__)
 
+    def query_string_to_dict(self, query_string):
+        roughdecode = dict(item.split('=', 1) for item in query_string.split('&'))
+        encoding = roughdecode.get('charset', None)
+
+        if encoding is None:
+            raise Http404
+
+        query = query_string.encode('ascii')
+        data = QueryDict(query, encoding=encoding).dict()
+
+        return data
+
     def close(self):
         super(AfterResponse, self).close()
 
         if self.status_code == 200:
             # now we do da shit
-            data = self.request.POST.copy()
-            data['cmd'] = "_notify-validate"
             url = ""
 
             if settings.DEBUG:
@@ -29,19 +41,24 @@ class AfterResponse(HttpResponse):
             else:
                 url = settings.PAYMENT['paypal']['live_ipn_endpoint']
 
+            query_string = self.request.body.decode('ascii')
+            data = self.query_string_to_dict(query_string)
+
+            response = urlopen(url, b("cmd=_notify-validate&%s" % query_string)).read()
+
             self.logger.debug("Let us try to validate IPN message.")
 
-            response = requests.post(url, data=data)
+            if response:
+                self.logger.debug("Just got response for IPN message.")
 
-            if response and response.status_code == requests.codes.ok:
-                self.logger.debug("Just got OK response for IPN message.")
-
-                if response.text == "VERIFIED":
+                if response == "VERIFIED":
                     self.logger.debug("IPN message VALID.")
 
-                    invoice_number = self.request.POST.get('invoice_number', '')
-                    invoice_id = self.request.POST.get('invoice_id', '')
-                    payment_status = self.request.POST.get('payment_status', '')
+                    invoice_number = data.get('invoice_number', '')
+                    invoice_id = data.get('invoice_id', '')
+                    payment_status = data.get('payment_status', '')
+
+                    self.logger.debug("We will check payment for invoice number %s, with invoice id %s and payment status %s" % (invoice_number, invoice_id, payment_status))
 
                     if invoice_id == '' or invoice_number == '':
                         raise Http404
