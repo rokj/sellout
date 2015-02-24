c21017f39d0f833f8830eded2e7f251c691626cf
Rok Jaklic <rokj@rasca.net>
2015-02-24 15:27:23 +0100

    scripts

--- bin/restart_pos.sh
+++ bin/restart_pos.sh
@@ -1,5 +1,9 @@
 #!/usr/local/bin/bash
 
+######
+###### sellout.biz
+######  
+
 # Replace these three settings.
 export DJANGO_SETTINGS_MODULE=settings
 PROJDIR="/usr/local/www/sellout_biz"
@@ -11,7 +15,7 @@ git pull
 
 if [ -f $PIDFILE ]; then
     # kill -9 `ps -aux | grep uwsgi | awk '{print $2}'
-    echo "Stopping server..."
+    echo "Stopping server for sellout.biz."
     uwsgi --stop $PIDFILE
     sleep 3
 fi
@@ -19,6 +23,6 @@ fi
 # echo "Removing .pyc files..."
 # rm `find . -name "*.pyc"`
 
-echo "Starting server..."
+echo "Starting server for sellout.biz."
 cd $PROJDIR
 uwsgi --wsgi-file /usr/local/www/sellout_biz/wsgi.py --python-path /usr/local/www/ --fastcgi-socket --ini pos.ini
