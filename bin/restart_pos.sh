5678ebd0d237cd862d91912601964b559111ea43
Rok Jaklič <rokj@rasca.net>
2015-04-16 14:23:21 +0200

    restarting

--- bin/restart_pos.sh
+++ bin/restart_pos.sh
@@ -16,7 +16,7 @@ git -c http.sslVerify=false pull
 if [ -f $PIDFILE ]; then
     # kill -9 `ps -aux | grep uwsgi | awk '{print $2}'
     echo "Stopping server for sellout.biz."
-    /usr/local/bin/uwsgi/uwsgi --stop $PIDFILE
+    /usr/local/bin/uwsgi --stop $PIDFILE
     sleep 3
 fi
 
@@ -25,7 +25,7 @@ fi
 
 echo "Starting server for sellout.biz."
 cd $PROJDIR
-/usr/local/bin/uwsgi/uwsgi --wsgi-file /usr/local/www/sellout_biz/wsgi.py --python-path /usr/local/www/ --fastcgi-socket --ini pos.ini
+/usr/local/bin/uwsgi --wsgi-file /usr/local/www/sellout_biz/wsgi.py --python-path /usr/local/www/ --fastcgi-socket --ini pos.ini
 
 echo "TESTS"
 echo "TESTS"
